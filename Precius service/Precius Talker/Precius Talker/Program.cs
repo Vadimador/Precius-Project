ee5cd69ab0e26ed3bafcf3e2601fe9b0c2f1555b
Vadimador <vadgolda@gmail.com>
2022-05-12 01:22:36 +0200

    Custom command show_modules and show_sectors added

---
+++ Precius service/Precius Talker/Precius Talker/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Precius_Talker
+{
+
+    class Program
+    {
+        public const string show_modules = "show_modules"; // montre la table des modules
+        public const string show_sectors = "show_sectors"; // montre la table des secteurs
+
+        public const string sourceName = "Précius";
+        public const string journalName = "precius-log";
+        public const float timerWait = 4;
+        static ServiceController Controller = null;
+        static EventLog el = null;
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (!initialiseServiceController())
+                {
+                    return;
+                }
+                initialiseArgLog();
+
+                switch (args[0])
+                {
+                    case show_modules:
+                       ShowModules();
+                        break;
+                    case show_sectors:
+                       ShowSectors();
+                        break;
+                    default:
+                        Console.WriteLine("Commande inconnue.");
+                        Console.WriteLine(help());
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine(help());
+            }
+        }
+
+        static public bool initialiseServiceController()
+        {
+            try
+            {
+                Controller = new ServiceController("Precius");
+                Console.WriteLine("Service Status : " + Controller.Status); // on l'utilise pour
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error have occur : " + e.Message);
+                Console.WriteLine(help());
+                return false;
+            }
+            return true;
+        }
+        static public void initialiseArgLog()
+        {
+            el = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists(sourceName))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(sourceName, journalName);
+            }
+            el.Source = sourceName;
+            el.Log = journalName;
+        }
+
+        static public void ShowSectors()
+        {
+            EventLogEntryCollection elec = el.Entries;
+            int counter = elec.Count - 1;
+            if (counter == -1)
+                counter = 0;
+
+            Controller.ExecuteCommand(128);
+            
+
+            float timer = timerWait;
+            Console.WriteLine("Waiting for service response... \n");
+            bool success = false;
+            while(timer > 0)
+            {
+                elec = el.Entries;
+
+                for (int i = counter; i < elec.Count; i++)
+                {
+                    //Console.WriteLine(elec[i].Message);
+                    if (elec[i].Message.Contains("CustomCommand.ShowSectors"))
+                    {
+                        Console.WriteLine(elec[i].Message);
+                        timer = 0;
+                        success = true;
+                        break;
+                    }
+                }
+                if (success)
+                    break;
+                Thread.Sleep(1000);
+                Console.WriteLine('.');
+                timer--;
+            }
+
+            if(!success)
+            {
+                Console.WriteLine("\nTime out !");
+            }
+        }
+
+        static public void ShowModules()
+        {
+            EventLogEntryCollection elec = el.Entries;
+            int counter = elec.Count - 1;
+            if (counter == -1)
+                counter = 0;
+
+            Controller.ExecuteCommand(129);
+
+
+            float timer = timerWait;
+            Console.WriteLine("Waiting for service response... \n");
+            bool success = false;
+            while (timer > 0)
+            {
+                elec = el.Entries;
+
+                for (int i = counter; i < elec.Count; i++)
+                {
+                    //Console.WriteLine(elec[i].Message);
+                    if (elec[i].Message.Contains("CustomCommand.ShowModules"))
+                    {
+                        Console.WriteLine(elec[i].Message);
+                        timer = 0;
+                        success = true;
+                        break;
+                    }
+                }
+                if (success)
+                    break;
+                Thread.Sleep(1000);
+                Console.WriteLine('.');
+                timer--;
+            }
+
+            if (!success)
+            {
+                Console.WriteLine("\nTime out !");
+            }
+        }
+
+        static public string help()
+        {
+            string h = "Something went wrong, here's the help : \ncommandes : show_sectors, show_modules";
+
+
+            return h;
+        }
+    }
+}
--- Precius service/Precius-service/Precius-service/Precius-service.cs
+++ Precius service/Precius-service/Precius-service/Precius-service.cs
@@ -16,6 +16,8 @@ namespace Precius_service
 {
     public partial class Precius : ServiceBase
     {
+        private int eventId = 1;
+
         // définition des différents mot clés
         public const string NAME = "name";
         public const string FILESCAN = "filescan";
@@ -42,13 +44,24 @@ namespace Precius_service
             }
             
         }
+        struct Precius_Module
+        {
+            public string filepath; // le chemin de l'executable/script du module
+            public string executable_chain; // la chaine d'execution du module
+            public string bad_output; // les mauvais output lors de son execution
+
+            public Precius_Module(string filepath, string executable_chain)
+            {
+                this.filepath = filepath;
+                this.executable_chain = executable_chain;
+                this.bad_output = "";
+            }
+        }
 
         private List<Sector> sectors = new List<Sector>(); // la liste de tous les sectors
-        private int eventId = 1;
-        // tab module
-        // tab secteurs
+        private IDictionary<string, Precius_Module> modules = new Dictionary<string,Precius_Module>();
 
-        
+
         private bool OutPutCheckFormat(string args)
         {
 
@@ -118,7 +131,7 @@ namespace Precius_service
             }
             catch (Exception e)
             {
-      
+                
                 eventLog1.WriteEntry("Une Exception à été catch ! " + e.Message, EventLogEntryType.Error, eventId++);
             }
             finally
@@ -127,11 +140,56 @@ namespace Precius_service
             }
         }
 
-        private void ShowSectors()
+        private void LoadModules()
         {
+            String all;
             try
             {
-                string seeSectors = "Affichage des secteurs : \n\n";
+                StreamReader sr = new StreamReader("C:\\Users\\vadgo\\Documents\\Precius\\module-config.txt");
+                all = sr.ReadToEnd();
+                eventLog1.WriteEntry("Lecture du fichier de config module.\n" + "config recu : \n" + all, EventLogEntryType.Information, eventId++);
+                all = all.Trim();
+                string[] tab = all.Split(new string[] { "[new module]" }, StringSplitOptions.None);
+                string[] subTab = { };
+
+                //eventLog1.WriteEntry("taille tableau : " + tab.Length , EventLogEntryType.Information, eventId++);
+
+
+                for (int i = 1; i < tab.Length; i++) // i est à 1 car il y à toujours un string vide
+                {
+                    //eventLog1.WriteEntry("i be like : " + i, EventLogEntryType.Information, eventId++);
+                    tab[i] = tab[i].Trim().Replace("\n\n", "");
+                    subTab = tab[i].Split('\n');
+
+                    
+                    if (subTab.Length == 3)
+                    {
+                        Precius_Module m = new Precius_Module(subTab[1].Trim(), subTab[2].Trim());  // on génére un module vide
+                        eventLog1.WriteEntry(subTab[0].Trim() + " file path : " + m.filepath, EventLogEntryType.Information, eventId++);
+                        eventLog1.WriteEntry(subTab[0].Trim() + " chaine executable : " + m.executable_chain, EventLogEntryType.Information, eventId++);
+
+                        this.modules.Add(subTab[0].Trim(),m);
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                eventLog1.WriteEntry("Une Exception à été catch ! - LoadModules" + e.Message, EventLogEntryType.Error, eventId++);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private string ShowSectors()
+        {
+            string seeSectors = "Affichage des secteurs : \n\n";
+            try
+            {
+               
                 for(int i = 0; i < sectors.Count(); i++)
                 {
                     seeSectors += "--------------------------------------------------------------\n";
@@ -149,7 +207,7 @@ namespace Precius_service
                         seeSectors += "     " + tabKeyWord[j] + "=" + sectors[i].rules[tabKeyWord[j]] + "\n";
                     }
                 }
-                eventLog1.WriteEntry(seeSectors, EventLogEntryType.Information, eventId++);
+                //eventLog1.WriteEntry(seeSectors, EventLogEntryType.Information, eventId++);
 
             }
             catch (Exception e)
@@ -160,6 +218,35 @@ namespace Precius_service
             {
 
             }
+            return seeSectors;
+        }
+
+        private string ShowModules()
+        {
+            string seeModules = "Affichage des modules : \n\n";
+            try
+            {
+                for (int i = 0; i < modules.Count(); i++)
+                {
+                    seeModules += "--------------------------------------------------------------\n";
+                    seeModules += " ====== nom modules : " + this.modules.ElementAt(i).Key + "\n";
+                    seeModules += "file path : " + this.modules.ElementAt(i).Value.filepath + "\n";
+                    seeModules += "chaine d'execution : " + this.modules.ElementAt(i).Value.executable_chain + "\n";
+                    seeModules += "bad outputs : " + this.modules.ElementAt(i).Value.bad_output + "\n";
+                }
+                //eventLog1.WriteEntry(seeModules, EventLogEntryType.Information, eventId++);
+                return seeModules;
+
+            }
+            catch (Exception e)
+            {
+                eventLog1.WriteEntry("Une Exception à été catch ! " + e.Message, EventLogEntryType.Error, eventId++);
+            }
+            finally
+            {
+
+            }
+            return seeModules;
         }
 
         public Precius()
@@ -187,7 +274,9 @@ namespace Precius_service
             timer.Start();
 
             LoadSectors(); // on génére les secteurs de précius
-            ShowSectors();
+            LoadModules(); // on génére les modules de précius
+            //ShowModules();
+            //ShowSectors();
         }
 
         public void OnTimer(object sender, ElapsedEventArgs args)
@@ -200,6 +289,32 @@ namespace Precius_service
         protected override void OnStop()
         {
             eventLog1.WriteEntry("In OnStop.");
+        }
+
+        protected override void OnCustomCommand(int command)
+        {
+            eventLog1.WriteEntry("Custom command reçu, numéro : " + command, EventLogEntryType.Information, eventId++);
+            string log = "";
+            switch (command)
+            {
+                case 128: // show sectors
+                    log = "CustomCommand.ShowSectors\n";
+                    eventLog1.WriteEntry(log + ShowSectors(), EventLogEntryType.SuccessAudit, eventId++);
+                    break;
+
+                case 129: // show modules
+                    log = "CustomCommand.ShowModules\n";
+                    eventLog1.WriteEntry(log + ShowModules(),EventLogEntryType.SuccessAudit, eventId++);
+                    break;
+                default:
+                    eventLog1.WriteEntry("Custom command inconnu reçu, numéro : " + command, EventLogEntryType.Error, eventId++);
+                    break;
+            }
+        }
+
+        private void eventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
+        {
+
         }
     }
 }
